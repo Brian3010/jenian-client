e27c8911497ed7e85f4a39160e5e6b8a16be5e05
Brian <48544476+Brian3010@users.noreply.github.com>
2026-03-04 13:15:11 +1100

    fix hellome dont pick up the filename

--- app/(private)/dashboard/page.tsx
+++ app/(private)/dashboard/page.tsx
@@ -1,15 +1,13 @@
 'use client';
 import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import Link from 'next/link';
-import React from 'react';
-import HelloMe from './components/hellome';
+import HelloMe from './components/HelloMe';
 
 export default function Dashboard() {
   return (
     <div className="w-full p-2">
       <div className="flex flex-col gap-3">
         <HelloMe />
-
         <div className="flex">
           <Link href="/chemist-warehouse" className="w-full max-w-xs">
             <Card className="w-full hover:bg-gray-100 cursor-pointer">
