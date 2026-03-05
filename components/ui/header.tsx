8b4c0fe91550885bd4770b17c6dc816626b0d839
Brian <48544476+Brian3010@users.noreply.github.com>
2026-03-06 02:39:05 +1100

    attach link to homepage

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { SidebarTrigger } from './sidebar';
 
 export default function Header() {
@@ -5,9 +6,12 @@ export default function Header() {
     <header className="block sticky top-0 z-20 border-b backdrop-blur border rounded-md">
       <div className="mx-auto flex items-center p-1 sm:p-2 lg:p-3">
         <SidebarTrigger className="md:hidden" />
-        <span className="flex-1 text-center font-semibold italic tracking-wide underline decoration-amber-500">
-          Jenian
-        </span>
+        <Link
+          href="/"
+          className="flex-1 text-center font-semibold italic tracking-wide underline decoration-amber-500 "
+        >
+          <span className="">Jenian</span>
+        </Link>
       </div>
     </header>
   );
