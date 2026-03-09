1ec2753939a0c43ad0d3df375522209eb2583c75
Brian <48544476+Brian3010@users.noreply.github.com>
2026-03-09 19:29:22 +1100

    add alert

--- app/(private)/chemist-warehouse/create-report/components/EodReportForm.tsx
+++ app/(private)/chemist-warehouse/create-report/components/EodReportForm.tsx
@@ -40,9 +40,11 @@ export default function EodReportForm() {
   const onSubmit = async (signInData: ReportValuesOutput) => {
     console.log('clicked clicked');
     console.log('🚀 ~ onSubmit ~ signInData:', signInData);
+    alert(`Report is being submitted, please wait... ${JSON.stringify(signInData)}`);
     try {
       setIsLoading(true);
       const data: { message: string; status: number } = await handleReport(signInData);
+      alert(data.message);
       console.log('🚀 ~ onSubmit ~ data:', data);
       setIsLoading(false);
       if (data.status === 200) {
@@ -54,7 +56,7 @@ export default function EodReportForm() {
     } catch (error) {
       console.error(error);
       notifyError('Failed to submit report. Please try again.');
-      // alert(error instanceof Error ? error.message : 'An unknown error occurred');
+      alert(error instanceof Error ? error + '|' + error.message : 'An unknown error occurred');
       setIsLoading(false);
       return router.push('/chemist-warehouse');
     }
