615fb0d7a355c63b3ad3016c847076b3e8821d3b
Brian <48544476+Brian3010@users.noreply.github.com>
2026-04-02 01:23:55 +1100

    Update: numeric mode input

--- app/(private)/chemist-warehouse/create-report/components/InputFieldAndError.tsx
+++ app/(private)/chemist-warehouse/create-report/components/InputFieldAndError.tsx
@@ -31,7 +31,12 @@ export default function InputFieldAndError({
       <Field key={item.registerName} className="flex-col items-center gap-1 text-base pb-4">
         <FieldLabel className="text-gray-700 w-44 sm:w-52">{item.itemName}</FieldLabel>
         <InputGroup className="py-5 rounded-xl">
-          <InputGroupInput {...register(item.registerName)} type={item.inputType} placeholder={item.helpText} />
+          <InputGroupInput
+            {...register(item.registerName)}
+            type={item.inputType}
+            placeholder={item.helpText}
+            inputMode={item.inputMode}
+          />
         </InputGroup>
         {fieldError && <p className="text-sm text-destructive mt-1">{fieldError}</p>}
       </Field>
