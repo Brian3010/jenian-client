0b0d83c0b13ce81620ebad49481f32eada95a560
Brian <48544476+Brian3010@users.noreply.github.com>
2026-06-10 04:40:33 +1000

    feat: refactor

--- features/shift/components/ShiftCalculatorCard.tsx
+++ features/shift/components/ShiftCalculatorCard.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { Card, CardDescription, CardHeader } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Card, CardAction, CardDescription, CardHeader } from '@/components/ui/card';
 import { TelegramIntegrationCardSkeleton } from '@/features/telegram/components/TelegramIntegrationCardSkeleton';
+import { formatDayMonth } from '@/lib/utils';
+import Link from 'next/link';
 import { usePayDetail } from '../context/PayDetailContext';
-import HasPayCycleState from './HasPayCycleState';
-import PayCycleRequiredState from './PayCycleRequiredState';
 
 export default function ShiftCalculatorCard() {
   const { payDetail, error, loading } = usePayDetail();
@@ -34,50 +35,86 @@ export default function ShiftCalculatorCard() {
   );
 }
 
-/*
-export default function ShiftCalculatorCard() {
-  const router = useRouter();
-  const [error, setError] = useState<string>('');
-  const [payCycleData, setPayCycleData] = useState<PayCycleResponse | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
+type HasPayCycleStateProps = {
+  payCycleData: {
+    payCycleStartDate: string;
+    payCycleEndDate: string;
+    shiftCountInCycle: number;
+    estimatedGrossPay: number;
+  };
+};
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const res = await getPayCycle();
-        console.log('🚀 ~ ShiftCalculatorCard ~ res:', res);
-        setPayCycleData(res);
-      } catch (error) {
-        console.error('Error fetching pay cycle:', error);
-        setError('Failed to load shift data. Please try again later.');
-      } finally {
-        setLoading(false);
-      }
-    })();
-  }, []);
-
-  if (error) {
-    return (
-      <Card className="p-5 flex flex-col gap-3">
-        <CardHeader className="p-0">
-          <div className="flex items-center justify-between">
-            <h1 className="text-base font-semibold text-gray-900">Shift Calculator</h1>
-          </div>
-          <div className="text-sm text-gray-500">Manage shifts and estimate your pay for the current cycle.</div>
-        </CardHeader>
-        <CardDescription className="flex flex-col gap-3 py-3 border-y">
-          <div className="text-sm text-red-500">{error}</div>
-        </CardDescription>
-      </Card>
-    );
-  }
-
-  if (loading) return <TelegramIntegrationCardSkeleton />;
-
-  return payCycleData && payCycleData.hasPayCycleSettings ? (
-    <HasPayCycleState payCycleData={payCycleData} />
-  ) : (
-    <PayCycleRequiredState />
+function HasPayCycleState({
+  payCycleData: { payCycleStartDate, payCycleEndDate, shiftCountInCycle, estimatedGrossPay },
+}: HasPayCycleStateProps) {
+  return (
+    <Card className="p-5 flex flex-col gap-3">
+      <CardHeader className="p-0">
+        <div className="flex items-center justify-between">
+          <h1 className="text-base font-semibold text-gray-900">Shift Calculator</h1>
+        </div>
+        <div className="text-sm text-gray-500">Manage shifts and estimate your pay for the current cycle.</div>
+      </CardHeader>
+      <CardDescription className="flex flex-col gap-3 py-3 border-y">
+        <DataTable
+          rows={[
+            {
+              label: 'Current Cycle',
+              value: `${formatDayMonth(payCycleStartDate)} - ${formatDayMonth(payCycleEndDate)}`,
+            },
+            {
+              label: 'Shifts Worked',
+              value: shiftCountInCycle.toString(),
+            },
+            {
+              label: 'Estimated Pay',
+              value: `$${estimatedGrossPay.toFixed(2)}`,
+            },
+          ]}
+        />
+      </CardDescription>
+      <CardAction className="w-full">
+        <Button className="w-full" variant="primary">
+          <Link href={'/chemist-warehouse/shift-calculator'}>
+            <span className="font-semibold">Open Shift Calculator</span>
+          </Link>
+        </Button>
+      </CardAction>
+    </Card>
   );
 }
-  */
+
+function DataTable({ rows }: { rows: { label: string; value: string }[] }) {
+  return (
+    <dl className="grid grid-cols-1 gap-y-2" data-slot="data-table">
+      {rows.map((row, index) => (
+        <div key={index} className="flex items-center justify-between">
+          <dt className="text-sm text-gray-400">{row.label}</dt>
+          <dd className="text-sm text-gray-900">{row.value}</dd>
+        </div>
+      ))}
+    </dl>
+  );
+}
+
+function PayCycleRequiredState() {
+  return (
+    <Card className="p-5 flex flex-col gap-3">
+      <CardHeader className="grid-rows-none p-0">
+        <div className="flex items-center justify-between">
+          <h1 className="text-base font-semibold text-gray-900">Shift Calculator</h1>
+          <span className="text-xs px-3 py-1 rounded-full font-medium bg-warning text-warning-text">
+            Setup Required
+          </span>
+        </div>
+        <div className="text-sm text-gray-500">Manage shifts and estimate your pay for the current cycle.</div>
+      </CardHeader>
+
+      <CardAction className="w-full">
+        <Button className="w-full" variant="primary">
+          <span className="font-semibold">Set up pay cycle</span>
+        </Button>
+      </CardAction>
+    </Card>
+  );
+}
