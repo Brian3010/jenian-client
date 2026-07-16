0a59fe97fe130231c3eb841b92f1098bb20e8551
Brian <48544476+Brian3010@users.noreply.github.com>
2026-07-16 11:10:10 +1000

    add: implement shift calculator

--- app/api/private/shift/bulks/route.ts
+++ app/api/private/shift/bulks/route.ts
@@ -1,14 +1,25 @@
 import { submitShifts } from '@/features/shift/services/shift.server';
-import { ShiftSummaryResult } from '@/features/shift/types';
+import { ShiftSummaryResult, UserShift } from '@/features/shift/types';
 import { ApiResponse } from '@/lib/api/api-types';
 import { appendSetCookieHeaders } from '@/lib/auth/cookie-headers';
 import { NextResponse } from 'next/server';
-
-export async function POST(request: Request) {
-  const { shifts, deletedShiftIds } = await request.json();
+type ShiftSubmitPayload = {
+  cycleStartDate: string;
+  cycleEndDate: string;
+  shifts: UserShift[];
+  deletedShiftIds: string[];
+};
+export async function PUT(request: Request) {
+  const { shifts, deletedShiftIds, cycleStartDate, cycleEndDate } = (await request.json()) as ShiftSubmitPayload;
+  console.log(`🚀 ~ PUT ~ { shifts, deletedShiftIds, cycleStartDate, cycleEndDate } :`, {
+    shifts,
+    deletedShiftIds,
+    cycleStartDate,
+    cycleEndDate,
+  });
 
   // Call the server-side feature function, which talks to ASP.NET.
-  const { serverResult, cookieHeaders } = await submitShifts(shifts, deletedShiftIds);
+  const { serverResult, cookieHeaders } = await submitShifts(cycleStartDate, cycleEndDate, shifts, deletedShiftIds);
 
   // Convert ServerResult to ApiResponse
   const apiResponseBody: ApiResponse<ShiftSummaryResult> = serverResult.ok
