abef06c2acaa44891562c944ceb0db88b8fa3094
Brian <48544476+Brian3010@users.noreply.github.com>
2026-07-30 02:53:08 +1000

    fix: update IsDemoUser handling to use string type; adjust session verification and requireSession return types

--- features/auth/services/auth.server.ts
+++ features/auth/services/auth.server.ts
@@ -29,7 +29,7 @@ export async function refreshAccessToken(cookieHeader: string): Promise<Response
 export async function logout(): Promise<ServerResult<void>> {
   const session = await verifySession();
   console.log('🚀 ~ logout ~ session:', session);
-  if (session.status === 'authenticated' && session.user.IsDemoUser) {
+  if (session.status === 'authenticated' && session.user.IsDemoUser === 'True') {
     console.log('🚀 ~ logout ~ session.user.IsDemoUser:', session.user.IsDemoUser);
     const { res } = await aspnetFetch('/api/Auth/demo-logout', {
       method: 'DELETE',
--- lib/auth/session.ts
+++ lib/auth/session.ts
@@ -82,7 +82,7 @@ export async function getAccessToken(): Promise<string | undefined> {
 export type SessionResult =
   | {
       status: 'authenticated';
-      user: Omit<UserPayload, 'IsDemoUser'> & { IsDemoUser: boolean };
+      user: UserPayload;
       accessToken: string;
     }
   | {
@@ -126,7 +126,7 @@ const verifySessionCache = cache(async (): Promise<SessionResult> => {
       user: {
         name: payload.name,
         email: payload.email,
-        IsDemoUser: payload.IsDemoUser?.toString() === 'True',
+        IsDemoUser: payload.IsDemoUser,
       },
     };
   } catch (error) {
@@ -154,9 +154,7 @@ export async function verifySession(): Promise<SessionResult> {
  * This is used in pages/layouts to ensure that the user is authenticated before rendering the page.
  * If the user is not authenticated, they will be redirected to the sign-in page.
  */
-export async function requireSession(
-  returnTo: string,
-): Promise<Omit<UserPayload, 'IsDemoUser'> & { IsDemoUser: boolean }> {
+export async function requireSession(returnTo: string): Promise<UserPayload> {
   const session = await verifySession();
 
   if (session.status === 'authenticated') {
